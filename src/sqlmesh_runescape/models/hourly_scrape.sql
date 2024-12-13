15978fbc456de95cab0b1fe2b1fbcbfb292941c8
Ben Epstein <ben.epstein97@gmail.com>
2024-12-13 15:15:44 -0500

    still werid

--- src/sqlmesh_runescape/models/hourly_scrape.sql
+++ src/sqlmesh_runescape/models/hourly_scrape.sql
@@ -8,16 +8,18 @@ MODEL (
     grain ('id', 'ts'),
     description 'Scrapes hourly price data from RuneScape API using UTC time as an integer.'
 );
+
+-- {% set url = "https://prices.runescape.wiki/api/v1/osrs/1h?timestamp=" %}
   
 SELECT
-    to_timestamp(ts) as ts,
+    ts,
     key AS item_id,
     json_extract(json_data, CONCAT('$.', key, '.avgHighPrice'))::INTEGER AS avg_high_price,
     json_extract(json_data, CONCAT('$.', key, '.highPriceVolume'))::INTEGER AS high_price_volume,
     json_extract(json_data, CONCAT('$.', key, '.avgLowPrice'))::INTEGER AS avg_low_price,
     json_extract(json_data, CONCAT('$.', key, '.lowPriceVolume'))::INTEGER AS low_price_volume
 FROM (
-    SELECT "timestamp" as ts, data::JSON AS json_data, 
+    SELECT to_timestamp("timestamp") as ts, data::JSON AS json_data, 
            UNNEST(json_keys(data::JSON)) AS key
-    FROM read_json_auto('https://prices.runescape.wiki/api/v1/osrs/1h')
+    FROM read_json_auto('https://prices.runescape.wiki/api/v1/osrs/1h?timestamp=' || epoch(TIMESTAMP '{{ start_ds }}'))
 );
